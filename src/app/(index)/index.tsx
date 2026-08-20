e63d48c665c6fd7b947688ee7004fbd62c7337c8
ferraricastro <filipecastrosaraiva@gmail.com>
2026-08-20 16:20:16 -0300

    add: header on main page

--- src/app/(index)/index.tsx
+++ src/app/(index)/index.tsx
@@ -1,12 +1,19 @@
-import { Text, View } from "react-native"
+import { MaterialIcons } from "@expo/vector-icons"
+import { Image, TouchableOpacity, View } from "react-native"
 
+import { colors } from "@/styles/colors"
 import { styles } from "./styles"
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
+      <View style={styles.header}>
+        <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
+        <TouchableOpacity activeOpacity={0.4}>
+          <MaterialIcons name="add" size={32} color={colors.blue[300]} />
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
-
--- src/app/(index)/styles.ts
+++ src/app/(index)/styles.ts
@@ -5,11 +5,22 @@ import { colors } from "@/styles/colors"
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    paddingTop: 52,
   },
   title: {
     color: colors.blue[900],
     fontSize: 22,
+  },
+  header: {
+    paddingHorizontal: 20,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 32,
+  },
+  logo: {
+    height: 32,
+    width: 32,
   },
 })
