b967159ad666dd92cff0ff3a5918b5ecd2cc7b24
ferraricastro <filipecastrosaraiva@gmail.com>
2026-09-22 18:12:33 -0300

    update: header and categories styles

--- src/app/(index)/styles.ts
+++ src/app/(index)/styles.ts
@@ -17,7 +17,7 @@ export const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    marginBottom: 32,
+    marginBottom: 10,
   },
   logo: {
     height: 32,
--- src/components/categories/styles.ts
+++ src/components/categories/styles.ts
@@ -2,8 +2,8 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    height: 52,
-    maxHeight: 52,
+    height: 42,
+    maxHeight: 42,
   },
   content: {
     gap: 14,
