de3009c317b8f60bc7d322038da42f938e70163f
ferraricastro <filipecastrosaraiva@gmail.com>
2026-09-22 10:55:17 -0300

    update: add all filter

--- src/components/categories/index.tsx
+++ src/components/categories/index.tsx
@@ -1,14 +1,22 @@
 import { FlatList } from "react-native"
 
-import { styles } from "./styles"
 import { Category } from "@/components/category"
 import { categories } from "@/utils/categories"
+import { styles } from "./styles"
 
 export function Categories() {
+  const filterCategories = [
+    {
+      id: null,
+      name: "Todos",
+      icon: "view-list" as const,
+    },
+    ...categories,
+  ]
   return (
     <FlatList
-      data={categories}
-      keyExtractor={(item) => item.id}
+      data={filterCategories}
+      keyExtractor={(item) => item.id ?? "all"}
       renderItem={({ item }) => (
         <Category name={item.name} icon={item.icon} isSelected={false} />
       )}
